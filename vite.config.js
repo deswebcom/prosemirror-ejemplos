a73d9adbdf2d538496a101889fe547f8c738d522
midesweb <malvarez@desarrolloweb.com>
2023-02-21 09:43:12 +0100

    boton para negrita

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         hello_world: resolve(__dirname, 'ejemplos', '01-hola-mundo-prosemirror.html'),
         commands: resolve(__dirname, 'ejemplos', '02-commandos-prosemirror.html'),
+        initEditor: resolve(__dirname, 'ejemplos', '03-inicializacion-documento.html'),
+        menu: resolve(__dirname, 'ejemplos', '04-bold-button.html'),
       },
     },
   },
