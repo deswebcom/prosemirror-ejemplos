6c9a880b80cb288c2a2b8ec20de7ad8b87fed983
midesweb <malvarez@desarrolloweb.com>
2023-02-23 11:13:41 +0100

    ejemplos de schemas

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
         plugins: resolve(__dirname, 'ejemplos', '05-plugins.html'),
         menu: resolve(__dirname, 'ejemplos', '06-menu.html'),
         serializar: resolve(__dirname, 'ejemplos', '07-serializar-html.html'),
-        
+        schemaList: resolve(__dirname, 'ejemplos', '08-schema-list.html'),
+        schemaMarkdown: resolve(__dirname, 'ejemplos', '09-schema-markdown.html'), 
       },
     },
   },
